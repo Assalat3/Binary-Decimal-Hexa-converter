89eca66360fd405175daf10e28df917a8ffd552e
Toni Ordning <34478752+Assalat3@users.noreply.github.com>
2019-03-24 16:32:23 +0200

    Add files via upload

---
+++ converter.java
@@ -0,0 +1,60 @@
+package BinHexaConverter;
+
+import javafx.scene.control.TextField;
+
+class converter {
+
+    static void convertBinary(String binary, TextField base10, TextField hexa){
+        try{
+
+            int decimalValue = Integer.parseInt(binary, 2);
+
+            String hexaValue = Integer.toHexString(decimalValue);
+
+            base10.setText(Integer.toString(decimalValue));
+            hexa.setText(hexaValue);
+
+            System.out.println(decimalValue + " " + hexaValue); //TROUBLESHOOT
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void convertDecimal(int decimal, TextField binary, TextField hexa){
+        try{
+
+            String binaryValue = Integer.toBinaryString(decimal);
+
+            String hexaValue = Integer.toHexString(decimal);
+
+            binary.setText(binaryValue);
+            hexa.setText(hexaValue);
+
+            System.out.println(binaryValue + " " + hexaValue);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void convertHexa(String hexa, TextField binary, TextField decimal){
+        try{
+
+            int convertedHexa = Integer.parseInt(hexa, 16);
+
+            String binaryValue = Integer.toBinaryString(convertedHexa);
+
+            String decimalValue = Integer.toString(convertedHexa);
+
+            binary.setText(binaryValue);
+            decimal.setText(decimalValue);
+
+            System.out.println(binaryValue + " " + decimalValue);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
